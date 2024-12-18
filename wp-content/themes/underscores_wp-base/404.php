36bad40806879c0edbfcb07003f0cbaa5acaf0b5
Yvan Kerdanet <yvan.kerdanet@gmail.com>
2024-12-18 17:13:47 +0100

    404 finish

--- wp-content/themes/underscores_wp-base/404.php
+++ wp-content/themes/underscores_wp-base/404.php
@@ -12,12 +12,13 @@ get_header();
 
 	<main id="prim_404">
 		<div id="div_404">
-			<h1>404</h1>
+			<h1 id="h1_404">404</h1>
 		</div>
 		<div id="div_oops">
-		<h2>Oups ! Cette page est introuvable</h2>
-		<button>Retourner à la page d'accueil<img src="/img/angle-petit-droit_vert.svg" alt="flèche droite verte" /></button>
-		
+			<h2>Oups ! Cette page est introuvable</h2>
+			<div>
+				<button>Retourner à la page d'accueil<img src="<?php echo get_template_directory_uri(); ?>/img/angle-petit-droit_vert.svg" alt="flèche droite verte" /></button>
+			</div>
 		</div>
 	</main>
 
