9179897fae53b45337838f6afe7a7745cc1ac463
Yezir971 <james_ahmedaly@yahoo.com>
2024-12-19 14:44:48 +0100

    landing page sans police james

--- wp-content/themes/underscores_wp-base/index.php
+++ wp-content/themes/underscores_wp-base/index.php
@@ -17,41 +17,24 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <article>
+         <header>
+			<h1 class="mainMenu">Un <span class="unedrlineMain">accompagnement</span> sur <span class="surroundedMain">mesure</span>  pour vos enfants</h1>
+			<p class="landingPresentation">“Chaque pas, même petit, est une victoire vers l'autonomie et la confiance en soi.”</p>
+			<button>Prendre rendez-vous</button>
+         <?php wp_body_open(); ?>
+            <div class="containerArticle"><?php the_title() ?></div>
+         </header>
+         <?php the_content(); ?>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+      </article>
+    <?php endwhile; else: ?>
+       <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
