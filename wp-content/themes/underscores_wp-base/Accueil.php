268e77fa9d379d5d3985784154f535fc0e84feb9
Yvan Kerdanet <yvan.kerdanet@gmail.com>
2024-12-19 16:31:34 +0100

    Ajout des fonctionnalitées principales

---
+++ wp-content/themes/underscores_wp-base/Accueil.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Accueil
+*/
+
+get_header();
+?>
+
+<main>
+    <h1>Accueil</h1>
+    <p>Accueil bla bla</p>
+</main>
+
+<?php get_footer(); ?>
--- wp-content/themes/underscores_wp-base/index.php
+++ wp-content/themes/underscores_wp-base/index.php
@@ -15,43 +15,24 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
+	<div class="container_articles">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
+			<div class="post">
+				<img src="<?php  if(has_post_thumbnail() ) : the_post_thumbnail_url(); else : echo get_template_directory_uri(),"/img/logo.png"; endif;?>" alt="">
+				<h1 class="title_post"><?php the_title(); ?></h1>
+				<p class="desc_post"><?php the_excerpt(); ?></p>
+				<p class="date_publish_post"><?php the_date(); ?></p>
+				<a href="<?php the_permalink(); ?>">Lire l'article </a>
+			</div>
 
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
+		<?php endwhile; else :
 
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
 		?>
+	</div>
+</main><!-- #main -->
 
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
