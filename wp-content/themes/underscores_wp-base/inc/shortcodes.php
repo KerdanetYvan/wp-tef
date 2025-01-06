5dac2bced66a03087532421e5b7513aad26a5487
Yvan Kerdanet <yvan.kerdanet@gmail.com>
2025-01-07 00:34:23 +0100

    Add new SVG images and landing page shortcode for 'Mon Parcours'

--- wp-content/themes/underscores_wp-base/inc/shortcodes.php
+++ wp-content/themes/underscores_wp-base/inc/shortcodes.php
@@ -536,8 +536,285 @@ function theme_custom_title_desc($atts) {
     <?php return ob_get_clean();
 }
 
+// Shortcode du landing de la page 'Mon Parcours'
+function theme_landing() {
+    ob_start(); ?>
+
+    <section class="landing">
+        <h1 class="titre_landing">Faisons connaissances</h1>
+        <div class="content_landing">
+            <p class="desc_landing">
+            Mon objectif ?<br /><span class="souligner aider">Aider</span> chaque enfant à surmonter ses difficultés avec le <span class="entourer">sourire</span>, dans une ambiance <span class="souligner">bienveillante</span> et adaptée à son rythme !
+            </p>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/madame.png" alt="Image de l'accompagnement" class="img_landing">
+        </div>
+    </section>
+    <style>
+        .landing {
+            background-color: #E5F2F1;
+            padding: 48px 40px 0 40px;
+            display: grid;
+            grid-template-columns: 100%;
+            gap: 24px;
+            margin-bottom: 48px;
+            width: 100vw;
+        }
+
+        .titre_landing {
+            width: 100%;
+            text-align: center;
+            font-size: 56px;
+            font-weight: 450;
+        }
+
+        .content_landing {
+            width: 100%;
+            display: grid;
+            grid-template-columns: 622px 364px;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .desc_landing {
+            font-size: 36px;
+            font-weight: 420;
+            max-width: 622px;
+        }
+
+        .img_landing {
+            width: 364px;
+            height: auto;
+        }
+
+        .entourer {
+            position: relative;
+        }
+
+        .entourer::after {
+            position: absolute;
+            content: "";
+            background: url('<?php echo get_template_directory_uri(); ?>/img/Ellipse.svg') no-repeat;
+            width: 125%;
+            height: 125%;
+            top: 7.5px;
+            left: -7.5px;
+        }
+
+        .souligner {
+            position: relative;
+        }
+
+        .souligner::after {
+            position: absolute;
+            content: "";
+            background: url('<?php echo get_template_directory_uri(); ?>/img/underline.svg') no-repeat;
+            width: 100%;
+            height: 10px;
+            bottom: -4px;
+            left: 0;
+        }
+
+        .aider::after {
+            background: url('<?php echo get_template_directory_uri(); ?>/img/Ellipse_7.svg') no-repeat;
+        }
+
+        @media screen and (max-width: 1024px) {
+            .content_landing {
+                grid-template-columns: 100%;
+            }
+
+            .img_landing {
+                display: none;
+            }
+        }
+    </style>
+
+    <?php return ob_get_clean();
+}
+
+// Shortcodes pour les petites sections de la page 'Mon Parcours'
+function theme_custom_section_desc($atts) {
+    $atts = shortcode_atts([
+        'type1' => '1',
+        'title1' => 'Titre de la section',
+        'description1' => 'Description de la section',
+        'type2' => '1',
+        'title2' => 'Titre de la section',
+        'description2' => 'Description de la section',
+        'type3' => '1',
+        'title3' => 'Titre de la section',
+        'description3' => 'Description de la section',
+    ], $atts);
+    ob_start(); ?>
+
+    <div class="container_section_desc">
+        <section class="section__desc">
+            <h3 class="titre_desc type<?= esc_html($atts['type1']); ?>"><?= esc_html($atts['title1']); ?></h3>
+            <p class="description"><?= esc_html($atts['description1']); ?></p>
+        </section>
+        <section class="section__desc">
+            <h3 class="titre_desc type<?= esc_html($atts['type2']); ?>"><?= esc_html($atts['title2']); ?></h3>
+            <p class="description"><?= esc_html($atts['description2']); ?></p>
+        </section>
+    </div>
+    <section class="section_desc">
+        <h3 class="titre_desc type<?= esc_html($atts['type3']); ?>"><?= esc_html($atts['title3']); ?></h3>
+        <p class="description"><?= esc_html($atts['description3']); ?></p>
+    </section>
+    <style>
+        .container_section_desc {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 40px;
+            padding: 48px 243px;
+        }
+
+        
+        .section_desc {
+            margin: 0;
+            padding: 48px 243px;
+        }
+        
+        @media screen and (max-width: 1024px) {
+            .container_section_desc {
+                grid-template-columns: 1fr;
+                gap: 96px;
+                padding-inline: 24px;
+
+            }
+
+            .titre_desc {
+                margin-left: 30px;
+            }
+
+            .section_desc {
+                padding-inline: 24px;
+            }
+        }
+
+        .titre_desc {
+            font-size: 24px;
+            font-weight: 700;
+            margin-bottom: 20px;
+            position: relative;
+        }
+
+        .titre_desc::after {
+            content: "";
+            position: absolute;
+            top: -40px;
+            left: -40px;
+            display: flex;
+            z-index: -1;
+        }
+        
+        .type1::after {
+            background: url('<?php echo get_template_directory_uri(); ?>/img/type1.svg') no-repeat;
+            width: 120px;
+            height: 90px;
+            transform: rotate(-3.513deg);
+            left: -35.438px;
+            top: -31.407px;
+        }
+
+        .type2::after {
+            background: url('<?php echo get_template_directory_uri(); ?>/img/type2.svg') no-repeat;
+            width: 120px;
+            height: 90px;
+            transform: rotate(18.014deg);
+            left: -48.824px;
+            top: -39.421px;
+        }
+
+        .type3::after {
+            background: url('<?php echo get_template_directory_uri(); ?>/img/type3.svg') no-repeat;
+            width: 120px;
+            height: 100px;
+            transform: rotate(-20.346deg);
+            left: -49.799px;
+            top: -49.517px;
+        }
+
+        .description {
+            text-align: justify;
+            font-size: 16px;
+            font-weight: 390;
+        }
+    </style>
+
+    <?php return ob_get_clean();
+}
+
+// Shortcode pour les sections 'Mon cabinet' et 'Séances'
+function theme_custom_green_fade($atts) {
+    $atts = shortcode_atts([
+        'title' => 'Titre de la section',
+        'desc' => 'Description de la section',
+        'type' => 'bureau',
+    ], $atts);
+    ob_start(); ?>
+
+    <section class="<?php echo esc_html($atts['type']); ?>">
+        <h2 class="titre_green_fade"><?= esc_html($atts['title']); ?></h2>
+        <p class="desc_green_fade"><?= esc_html($atts['desc']); ?></p>
+    </section>
+    <style>
+        .bureau {
+            width: calc(100% - 486px);
+            height: 232px;
+            margin-inline: 243px;
+            padding: 24px;
+            background: linear-gradient(90deg, #B9DADA 10.33%, rgba(52, 144, 148, 0.00) 100%), linear-gradient(90deg, rgba(255, 255, 255, 0.40) 0%, rgba(255, 255, 255, 0.00) 60%, rgba(255, 255, 255, 0.00) 99.89%), url('<?php echo get_template_directory_uri(); ?>/img/bureau.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            border-radius: 20px;
+        }
+
+        .seance {
+            width: calc(100% - 486px);
+            height: 232px;
+            margin-inline: 243px;
+            padding: 24px;
+            background: linear-gradient(90deg, #B9DADA 10.33%, rgba(52, 144, 148, 0.00) 100%), linear-gradient(90deg, rgba(255, 255, 255, 0.40) 0%, rgba(255, 255, 255, 0.00) 60%, rgba(255, 255, 255, 0.00) 99.89%), url('<?php echo get_template_directory_uri(); ?>/img/seance.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            border-radius: 20px;
+        }
+
+        .titre_green_fade {
+            font-size: 24px;
+            font-weight: 700;
+        }
+
+        .desc_green_fade {
+            font-size: 16px;
+            font-weight: 390;
+            text-align: justify;
+            width: 50%;
+        }
+
+        @media screen and (max-width: 1024px) {
+            .bureau, .seance {
+                width: calc(100% - 48px);
+                margin-inline: 24px;
+            }
+
+            .desc_green_fade {
+                width: 100%;
+            }
+        }
+    </style>
+
+    <?php return ob_get_clean();
+}
+
 add_shortcode('custom_section_services', 'theme_custom_section_services_shortcode');
 add_shortcode('custom_section_between', 'theme_custom_section_between_shortcode');
 add_shortcode('custom_service', 'theme_custom_service');
 add_shortcode('custom_stage', 'theme_custom_stage');
 add_shortcode('custom_title_desc', 'theme_custom_title_desc');
+add_shortcode('landing', 'theme_landing');
+add_shortcode('custom_section_desc', 'theme_custom_section_desc');
+add_shortcode('custom_green_fade', 'theme_custom_green_fade');
